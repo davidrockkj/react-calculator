0256329d8a3aaf9e6ea73edeacff0db0c3971d44
David Rocha <davidrockkj@gmail.com>
2024-03-10 19:02:47 -0300

    Button: 'flex: 1, color: 1', '&:hover'

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -1,13 +1,16 @@
 import styled from "styled-components";
 
 export const ButtonContainer = styled.button`
+  flex: 1;
   padding: 20px;
   border: 1px solid #cdcdcd;
   background-color: #00aaff;
+
+  color: #fff;
   font-size: 24px;
   font-weight: bold;
 
   &:hover {
-    background-color: #00aa;
+    opacity: 0.6;
   }
 `
