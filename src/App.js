25fffa4c4b2b4d50224e25e92f8ac0231952130d
David Rocha <davidrockkj@gmail.com>
2024-03-10 20:18:59 -0300

    Criando o 'handleEquals'

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import { Container, Content, Row } from "./styles";
 import { useState } from "react";
 
 const App = () => {
-  const [currentNumber, setCurrentNumber] = useState('');
+  const [currentNumber, setCurrentNumber] = useState('0');
   const [firstNumber, setFirstNumber] = useState('0');
+  const [operation, setOperation] = useState('');
 
   const handleOnClear = () => {
     setCurrentNumber('');
+    setFirstNumber('');
+    setOperation('');
   }
 
   const handleAddNumber = (number) => {
@@ -17,10 +20,19 @@ const App = () => {
 
   const handleSumNumbers = () => {
     if (firstNumber === '0') {
-      setFirstNumber(currentNumber);
+      setFirstNumber('0');
+      setOperation('+');
     }else{
       const sum = Number(firstNumber) + Number(currentNumber);
       setCurrentNumber(String(sum));
+      setOperation('');
+    }
+  }
+
+  const handleEquals = () => {
+    if (!firstNumber === '0' && operation !== '') {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0');
     }
   }
 
@@ -51,7 +63,7 @@ const App = () => {
           <Button label={1} onClick={ () => handleAddNumber('1') } />
           <Button label={2} onClick={ () => handleAddNumber('2') } />
           <Button label={3} onClick={ () => handleAddNumber('3') } />
-          <Button label={'='} onClick={ () => handleAddNumber('=') } />
+          <Button label={'='} onClick={ handleEquals } />
         </Row>
         <Row>
           <Button label={0} onClick={ () => handleAddNumber('0') } />
